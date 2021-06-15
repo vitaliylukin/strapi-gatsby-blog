484ddc429e5812a38821e1c32c425bd702e3f122
Vitaliy Lukin <vitaliylukin91@gmail.com>
2021-06-15 19:51:43 +0300

    first commit

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7f6ba8af81e407c9449e323450bfc0eb'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'df848180-3384-4f64-bb5c-a86759885f27'
+};
